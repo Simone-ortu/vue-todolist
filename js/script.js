7bab0ce46bee3b8ba9a5534a920404f158e196a4
Simone-Ortu <simo.28.09o@gmail.com>
2021-01-28 15:42:41 +0100

    methods

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert('boh')
+var app = new Vue({
+    el:'#app',
+    data:{
+        todos:[
+            'Fare la spesa',
+            'Fare le commissioni',
+            'Preparare la cena',
+            'Pulire'
+        ],
+        todosDelete: []
+    },
+    methods:{
+        deleteTodo(index){
+            this.todos.splice(index,1)
+            this.todosDelete.push(this.todos[index])
+
+        }
+    }
+
+});
